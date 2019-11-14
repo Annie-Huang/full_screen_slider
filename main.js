667d6a8ba21dca3ac630b275e5767cf313c3e013
Annie Huang <annie.hn@gmail.com>
2019-11-14 22:17:28 +1100

    Set basic variables in in main.js and add nextSlide method.

---
+++ main.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+
+    // Check for next slide, check if it has the next sibling element
+    if (current.nextElementSibling) {
+        // Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        // Add current to start
+        slides[0].classList.add('current');
+    }
+
+    // I don't think the follow part is required. A lot of the comment in the video said it was not required.
+    setTimeout(() => current.classList.remove('current'), 200);
+};
